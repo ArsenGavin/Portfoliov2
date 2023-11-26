5a7e044ecea2366e4d31ce381f0cbeb9a05c6ee2
Thomas Birmingham <gavin8birmingham@yahoo.com>
2023-11-26 16:05:15 +0100

    correction responsive

--- src/Components/Home/Projects/Projects.js
+++ src/Components/Home/Projects/Projects.js
@@ -3,56 +3,62 @@ import './Projects.css';
 function Projects({ colorPoliceCanvas }) {
   return (
     <div className="projectContainer">
-      <div
-        className={`cardProject ${
-          colorPoliceCanvas === '#FE4760'
-            ? 'cardProject1'
-            : colorPoliceCanvas === '#F76B15'
-            ? 'cardProject2'
-            : colorPoliceCanvas === '#0F0'
-            ? 'cardProject3'
-            : colorPoliceCanvas === '#007bff'
-            ? 'cardProject4'
-            : colorPoliceCanvas === '#6f42c1'
-            ? 'cardProject5'
-            : ''
-        }`}
-      >
-        <p className="heading">Project 1</p>
+      <div className="cardBox">
+        <div
+          className={`cardProject ${
+            colorPoliceCanvas === '#FE4760'
+              ? 'cardProject1'
+              : colorPoliceCanvas === '#F76B15'
+              ? 'cardProject2'
+              : colorPoliceCanvas === '#0F0'
+              ? 'cardProject3'
+              : colorPoliceCanvas === '#007bff'
+              ? 'cardProject4'
+              : colorPoliceCanvas === '#6f42c1'
+              ? 'cardProject5'
+              : ''
+          }`}
+        >
+          <p className="heading">Project 1</p>
+        </div>
       </div>
-      <div
-        className={`cardProject ${
-          colorPoliceCanvas === '#FE4760'
-            ? 'cardProject1'
-            : colorPoliceCanvas === '#F76B15'
-            ? 'cardProject2'
-            : colorPoliceCanvas === '#0F0'
-            ? 'cardProject3'
-            : colorPoliceCanvas === '#007bff'
-            ? 'cardProject4'
-            : colorPoliceCanvas === '#6f42c1'
-            ? 'cardProject5'
-            : ''
-        }`}
-      >
-        <p className="heading">Project 2</p>
+      <div className="cardBox">
+        <div
+          className={`cardProject ${
+            colorPoliceCanvas === '#FE4760'
+              ? 'cardProject1'
+              : colorPoliceCanvas === '#F76B15'
+              ? 'cardProject2'
+              : colorPoliceCanvas === '#0F0'
+              ? 'cardProject3'
+              : colorPoliceCanvas === '#007bff'
+              ? 'cardProject4'
+              : colorPoliceCanvas === '#6f42c1'
+              ? 'cardProject5'
+              : ''
+          }`}
+        >
+          <p className="heading">Project 2</p>
+        </div>
       </div>
-      <div
-        className={`cardProject ${
-          colorPoliceCanvas === '#FE4760'
-            ? 'cardProject1'
-            : colorPoliceCanvas === '#F76B15'
-            ? 'cardProject2'
-            : colorPoliceCanvas === '#0F0'
-            ? 'cardProject3'
-            : colorPoliceCanvas === '#007bff'
-            ? 'cardProject4'
-            : colorPoliceCanvas === '#6f42c1'
-            ? 'cardProject5'
-            : ''
-        }`}
-      >
-        <p className="heading">Project 3</p>
+      <div className="cardBox">
+        <div
+          className={`cardProject ${
+            colorPoliceCanvas === '#FE4760'
+              ? 'cardProject1'
+              : colorPoliceCanvas === '#F76B15'
+              ? 'cardProject2'
+              : colorPoliceCanvas === '#0F0'
+              ? 'cardProject3'
+              : colorPoliceCanvas === '#007bff'
+              ? 'cardProject4'
+              : colorPoliceCanvas === '#6f42c1'
+              ? 'cardProject5'
+              : ''
+          }`}
+        >
+          <p className="heading">Project 3</p>
+        </div>
       </div>
     </div>
   );
